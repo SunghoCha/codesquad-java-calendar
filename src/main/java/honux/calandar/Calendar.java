10fa8119ece8172a1941bcf7db59b2957153f806
SungHo Cha <tjdgh3725@gmail.com>
2023-09-03 02:56:22 +0900

    가상의 달력찍기1

--- src/main/java/honux/calandar/Calendar.java
+++ src/main/java/honux/calandar/Calendar.java
@@ -11,15 +11,34 @@ public class Calendar {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String PROMPT = "cal> ";
 
-        System.out.println("반복 횟수를 입력하세요.");
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            System.out.printf("달을 입력하세요 [%d/%d]\n", i,N);
-            int month = Integer.parseInt(br.readLine());
+//        System.out.println("반복 횟수를 입력하세요.");
+
+//        int N = Integer.parseInt(br.readLine());
+//        for (int i = 0; i < N; i++) {
+//            System.out.printf("달을 입력하세요 [%d/%d]\n", i,N);
+//            System.out.print(PROMPT);
+//            int month = Integer.parseInt(br.readLine());
+//            Calendar cal = new Calendar();
+//
+//            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.FindMaxDay(month));
+//        }
+
+        int month = 0;
+        while (month != -1) {
+            System.out.println("달을 입력하세요.");
+            System.out.print(PROMPT);
+            month = Integer.parseInt(br.readLine());
+
+            if (month == -1) {
+                System.out.println("입력을 종료합니다.");
+                break;
+            }
+
             Calendar cal = new Calendar();
-
             System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.FindMaxDay(month));
+            cal.PrintCalander(cal.FindMaxDay(month));
         }
 
 
@@ -29,13 +48,13 @@ public class Calendar {
         return maxDays[month-1];
         }
 
-    public void PrintSample() {
+    public void PrintCalander(int maxDay) {
 
         StringBuilder sb = new StringBuilder();
 
         System.out.println(" 일 월 화 수 목 금 토");
         System.out.println("--------------------");
-        for (int i = 1; i < 29; i++) {
+        for (int i = 1; i <= maxDay; i++) {
             if (i < 10 && i % 7 == 0) {
                 sb.append(" ").append(i).append(" ").append("\n");
             } else if(i < 10){
