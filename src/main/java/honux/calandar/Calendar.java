b3b932d959619dcdf655e42ff8438d5021788fc2
SungHo Cha <tjdgh3725@gmail.com>
2023-09-03 03:20:16 +0900

    가상의 달력찍기1 수정본1

--- src/main/java/honux/calandar/Calendar.java
+++ src/main/java/honux/calandar/Calendar.java
@@ -8,50 +8,16 @@ public class Calendar {
 
     private final int[] maxDays = {31, 28, 31, 30 ,31, 30, 31, 31, 30, 31, 30, 31};
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String PROMPT = "cal> ";
-
-//        System.out.println("반복 횟수를 입력하세요.");
-
-//        int N = Integer.parseInt(br.readLine());
-//        for (int i = 0; i < N; i++) {
-//            System.out.printf("달을 입력하세요 [%d/%d]\n", i,N);
-//            System.out.print(PROMPT);
-//            int month = Integer.parseInt(br.readLine());
-//            Calendar cal = new Calendar();
-//
-//            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.FindMaxDay(month));
-//        }
-
-        int month = 0;
-        while (month != -1) {
-            System.out.println("달을 입력하세요.");
-            System.out.print(PROMPT);
-            month = Integer.parseInt(br.readLine());
-
-            if (month == -1) {
-                System.out.println("입력을 종료합니다.");
-                break;
-            }
-
-            Calendar cal = new Calendar();
-            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.FindMaxDay(month));
-            cal.PrintCalander(cal.FindMaxDay(month));
-        }
-
-
-    }
-
-    public int FindMaxDay(int month){
+    public int findMaxDay(int month){
         return maxDays[month-1];
         }
 
-    public void PrintCalander(int maxDay) {
+    public void printCalander(int year, int month) {
 
+        int maxDay = findMaxDay(month);
         StringBuilder sb = new StringBuilder();
 
+        System.out.printf ("     %4d년%2d월\n", year, month);
         System.out.println(" 일 월 화 수 목 금 토");
         System.out.println("--------------------");
         for (int i = 1; i <= maxDay; i++) {
---
+++ src/main/java/honux/calandar/Prompt.java
@@ -0,0 +1,53 @@
+package honux.calandar;
+
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Prompt {
+
+    private final String PROMPT = "cal> ";
+    public static void main(String[] args) throws IOException {
+
+        Prompt p = new Prompt();
+        p.runPrompt();
+    }
+
+    public void runPrompt() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+//        System.out.println("반복 횟수를 입력하세요.");
+
+//        int N = Integer.parseInt(br.readLine());
+//        for (int i = 0; i < N; i++) {
+//            System.out.printf("달을 입력하세요 [%d/%d]\n", i,N);
+//            System.out.print(PROMPT);
+//            int month = Integer.parseInt(br.readLine());
+//            Calendar cal = new Calendar();
+//
+//            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.FindMaxDay(month));
+//        }
+
+        int month = 0;
+        while (month != -1) {
+            System.out.println("달을 입력하세요.");
+            System.out.print(PROMPT);
+            month = Integer.parseInt(br.readLine());
+
+            if (month == -1) {
+                System.out.println("입력을 종료합니다.");
+                break;
+            }
+
+            Calendar cal = new Calendar();
+
+            cal.printCalander(2023,month);
+            System.out.println();
+        }
+
+
+    }
+
+}
