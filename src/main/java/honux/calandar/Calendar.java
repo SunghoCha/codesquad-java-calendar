d646be4b364a4e60918a445a6566afd0836a107d
SungHo Cha <tjdgh3725@gmail.com>
2023-09-03 01:55:08 +0900

    println 사용해서 출력하기 (조건문 사용해서 출력)

--- src/main/java/honux/calandar/Calendar.java
+++ src/main/java/honux/calandar/Calendar.java
@@ -2,7 +2,25 @@ package honux.calandar;
 
 public class Calendar {
     public static void main(String[] args) {
-        System.out.println("Hello, Calendar");
+
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println(" 일 월 화 수 목 금 토");
+        System.out.println("--------------------");
+        for (int i = 1; i < 29; i++) {
+            if (i < 10 && i % 7 == 0) {
+                sb.append(" ").append(i).append(" ").append("\n");
+            } else if(i < 10){
+                sb.append(" ").append(i).append(" ");
+            } else if(i > 10 && i % 7 == 0){
+                sb.append(i).append(" ").append("\n");
+            } else {
+                sb.append(i).append(" ");
+            }
+        }
+        String s = sb.toString();
+        System.out.println(s);
+
 
     }
 }
